c494d4d8b9309dad00a82343fb0fa985d68c8299
Ganesh Dahal <92071941+GaneshXd@users.noreply.github.com>
2022-11-14 18:05:23 +0545

    addfile

---
+++ swap.java
@@ -0,0 +1,9 @@
+import java.util.*;  
+class Swap   
+  public static void main(String a[])   
+    {   
+        System.out.println("Enter the value of x and y");  
+        Scanner sc = new Scanner(System.in);  
+        /*Define variables*/  
+        int x = sc.nextInt();  
+        int y = sc.nextInt();  
